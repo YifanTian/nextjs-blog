6374a42c644219c9805cdf7ca845ef88cc2458fd
Yifan Tian <yifant@Yifans-iMac.local>
2023-08-17 22:16:29 -0400

    analytics

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { Analytics } from '@vercel/analytics/react';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -11,7 +13,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read <Link href="/posts/first-post">this page!</Link>
         </h1>
 
         <p className={styles.description}>
@@ -47,6 +49,7 @@ export default function Home() {
             </p>
           </a>
         </div>
+        <Analytics />
       </main>
 
       <footer>
